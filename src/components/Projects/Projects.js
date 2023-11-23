298f6b53aa362bc6dc44dfe23541c42cfe3d015f
Lordkaito <cespedesisai88@gmail.com>
2023-11-23 22:41:46 +0100

    Update projects, fix CV

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
   return (
@@ -21,6 +15,26 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              // imgPath={chatify}
+              isBlog={false}
+              title="Web Crawler"
+              description="A web crawler built with JavaScript using puppeteer. It using the ycombinator website as a base, extracts information from the articles and allows you to filter them by comments, points or show them all."
+              ghLink="https://github.com/Lordkaito/web-crawler"
+              // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              // imgPath={chatify}
+              isBlog={false}
+              title="Multichattt"
+              description="A website built to allow you to see many twitch chats at the same time, including the streams of those users. Built with React and Typescript, it uses the twitch API to get the information of the streams and the chat."
+              ghLink="https://github.com/Lordkaito/multichattt"
+              // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               // imgPath={chatify}
@@ -64,29 +78,6 @@ function Projects() {
               // demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
-
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              // imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              // imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
-            />
-          </Col> */}
         </Row>
       </Container>
     </Container>
